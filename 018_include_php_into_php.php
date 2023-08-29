2e84ced6bf216170cc09c6afb41c65332c93cc2c
turkertunay <75833839+turkertunay@users.noreply.github.com>
2023-08-29 23:57:54 +0300

    Add files via upload

---
+++ 018_include_php_into_php.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  <?php
+    $title = "my first post";
+    $author = "iiii";
+    $wordCount = 600;
+    include "article-header.php";
+
+    include "useful-tools.php";
+    sayHi("iiii");
+    echo "<br>";
+
+    echo $feetInMile;
+   ?>
+
+
+
+
+
+  </body>
+</html>
---
+++ article-header.php
@@ -0,0 +1,3 @@
+<h2><?php echo $title; ?></h2>
+<h4><?php echo $author; ?></h4>
+word count: <h4><?php echo $wordCount; ?></h4>
